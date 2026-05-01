ff3235dc06b035298dc59aabb4a0f42089bc679f
allivan2020 <allivan2020@gmail.com>
2026-05-01 16:40:14 +0300

    feat: complete express app setup with routes and middlewares

--- src/server.js
+++ src/server.js
@@ -1,15 +1,51 @@
-// src/server.js
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
+
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes'
+  });
+});
+ 
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`
+  });
 });
 
-// Запуск сервера
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
